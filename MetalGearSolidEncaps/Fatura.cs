f12b1b312efefe3421f44a04a28e39d76de7e386
Daniel Matias da Silva <daniel.silva@leanwork.com.br>
2022-11-12 10:44:56 -0300

    Adicionar arquivos de projeto.

---
+++ MetalGearSolidEncaps/Fatura.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetalGearSolidEncaps
+{
+    public class Fatura
+    {
+        public string Cliente { get; private set; }
+
+        public double Valor { get; set; }
+
+        private IList<Pagamento> pagamentos;
+
+        
+
+        public bool Pago { get; private set; }
+
+        public Fatura(string cliente, double valor)
+        {
+            this.Cliente = cliente;
+            this.Valor = valor;
+            this.pagamentos = new List<Pagamento>();
+            this.Pago = false;
+        }
+
+        public void AdicionaPagamento(Pagamento pagamento)
+        {
+            this.pagamentos.Add(pagamento);
+
+            if(valorTotalPagamentos() >= Valor)
+            {
+                this.Pago = true;
+            }
+        }
+
+        private double valorTotalPagamentos()
+        {
+            double total = 0;
+
+            foreach(Pagamento pag in pagamentos)
+            {
+                total += pag.Valor;
+            }
+
+            return total;
+        } 
+    }
+}
---
+++ MetalGearSolidEncaps/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MetalGearSolidEncaps
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Boleto>boletos= new List<Boleto>();
+            boletos.Add(new Boleto(200));
+            boletos.Add(new Boleto(300));
+            boletos.Add(new Boleto(400));
+
+            Fatura fatura = new Fatura("Daniel", 900);
+
+            ProcessaBoleto pdb = new ProcessaBoleto();
+
+            pdb.Processa(boletos, fatura);
+
+            Console.WriteLine(fatura.Pago);
+            Console.ReadKey();
+        }
+    }
+}
